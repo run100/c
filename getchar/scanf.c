4c6e35effb22db31c333434c1d23736a5741ae75
run <727271755@qq.com>
2015-04-21 19:37:50 +0800

    getchar

---
+++ getchar/scanf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+	
+    int a,b,c;
+	/*printf("input a,b,c\n");
+    scanf("%d%d%d",&a,&b,&c);
+    printf("a=%d,b=%d,c=%d\n",a,b,c);
+	*/
+	
+	int d,e;
+	printf("input d\n");
+	scanf("%4d%4d", &d, &e);
+	printf("d=%d,e=%d\n", d, e);
+	return 0;
+}
