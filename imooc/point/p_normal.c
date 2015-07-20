8caf9b25bea12e581a889851f6f72d372e034c41
imac <chromev@qq.com>
2015-07-20 20:37:30 +0800

    imooc

---
+++ imooc/point/p_normal.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void change(int a, int b)
+{
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
+
+int main()
+{
+	int a = 5;
+	int b = 3;
+	change(a, b);	
+	printf("a is %d, b is %d", a, b);	
+	return 0;
+}
